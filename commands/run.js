cc06e6142372535736abc28ef0d9f30dc6f08843
qbao <dangquocbao9504@gmail.com>
2025-02-08 14:07:33 +0700

    !run works with php, python

--- commands/run.js
+++ commands/run.js
@@ -1,5 +1,38 @@
+const { EmbedBuilder, Colors } = require("discord.js");
+const Commander = require("../store/model/cmder.js");
+
 const run = async (message, lang, source) => {
-    await message.reply("hi");
+    const cmder = new Commander(lang, source);
+    await cmder.execute();
+    const result = cmder.result();
+
+    const color = result.output ? Colors.Green : Colors.Red;
+    const outputEmbed = new EmbedBuilder()
+        .setColor(color)
+        .setTitle("Remote Code Execution")
+        .setTimestamp()
+        .addFields({
+            name: "Output",
+            value: `\`\`\`${result.language.name}\n${result.output}\`\`\``,
+        })
+        .addFields(
+            {
+                name: "Time",
+                value: `${result.time} s`,
+                inline: true,
+            },
+            {
+                name: "Memory",
+                value: `${Math.round(result.memory / 1024)} MB`,
+                inline: true,
+            }
+        )
+        .setFooter({
+            text: `${result.language.version} | ${result.status.description}`,
+            iconURL: `${result.language.icon}`,
+        });
+
+    await message.reply({ embeds: [outputEmbed] });
 };
 
 module.exports = run;
--- store/model/cmder.js
+++ store/model/cmder.js
@@ -3,23 +3,64 @@ const { request } = require("undici");
 const { encodeBase64, decodeBase64 } = require("../../utils.js");
 
 const languageMap = {
-    python: 71, // Python (3.8.1)
-    cplusplus: 76, // C++ (Clang 7.0.1)
-    golang: 60, // Go (1.13.5)
-    java: 62, // Java (OpenJDK 13.0.1)
-    javascript: 63, // JavaScript (Node.js 12.14.0)
-    php: 68, // PHP (7.4.1)
-    go: 60, // Go (1.13.5)
-    bash: 46, // Bash (5.0.0)
+    python: {
+        id: 71,
+        name: "python",
+        version: "Python (3.8.1)",
+        icon: "https://i.imgur.com/N4RyEvG.png",
+    },
+    cplusplus: {
+        id: 54,
+        name: "cplusplus",
+        version: "C++ (GCC 9.2.0)",
+        icon: "https://i.imgur.com/CJYqkG5.png",
+    },
+    golang: {
+        id: 60,
+        name: "golang",
+        version: "Go (1.13.5)",
+        icon: "https://i.imgur.com/a3yrHtU.png",
+    },
+    java: {
+        id: 62,
+        name: "java",
+        version: "Java (OpenJDK 13.0.1)",
+        icon: "https://i.imgur.com/5OwBztX.png",
+    },
+    javascript: {
+        id: 63,
+        name: "javascript",
+        version: "JavaScript (Node.js 12.14.0)",
+        icon: "https://i.imgur.com/YOsQqBF.png",
+    },
+    php: {
+        id: 68,
+        name: "php",
+        version: "PHP (7.4.1)",
+        icon: "https://i.imgur.com/cnnYSIE.png",
+    },
+    go: {
+        id: 95,
+        name: "go",
+        version: "Go (1.18.5)",
+        icon: "https://i.imgur.com/a3yrHtU.png",
+    },
+    bash: {
+        id: 46,
+        name: "bash",
+        version: "Bash (4.4)",
+        icon: "https://i.imgur.com/6BQ4g4J.png",
+    },
 };
 
 class Commander {
     constructor(language, sourceCode, stdin = null) {
-        this.languageID = languageMap[language];
-        this.language = language;
+        this.languageID = languageMap[language].id;
+        this.language = languageMap[language];
         this.sourceCode = sourceCode;
         this.input = stdin;
 
+        this.status = null;
         this.output = null;
         this.time = null;
         this.memory = null;
@@ -29,6 +70,7 @@ class Commander {
         this.baseURI = `${this.judgeURI}/submissions`;
         this.token = "";
     }
+
     async execute() {
         const reqBody = {
             source_code: encodeBase64(this.sourceCode),
@@ -51,6 +93,20 @@ class Commander {
         await this._pollForResult();
     }
 
+    result() {
+        return {
+            languageID: this.languageID,
+            language: this.language,
+            sourceCode: this.sourceCode,
+            input: this.input,
+            output: this.output,
+            time: this.time,
+            memory: this.memory,
+            error: this.error,
+            status: this.status,
+        };
+    }
+
     async _pollForResult() {
         let result;
         while (true) {
@@ -65,29 +121,17 @@ class Commander {
 
     async _getResult() {
         const { body } = await request(
-            `${this.baseURI}/${this.token}?base64_encoded=true&fields=stdout,stderr,time,memory`
+            `${this.baseURI}/${this.token}?base64_encoded=true&fields=stdout,stderr,time,memory,status`
         );
         return await body.json();
     }
 
-    result() {
-        return {
-            languageID: this.languageID,
-            language: this.language,
-            sourceCode: this.sourceCode,
-            input: this.input,
-            output: this.output,
-            time: this.time,
-            memory: this.memory,
-            error: this.error,
-        };
-    }
-
     _processOutput(body) {
         this.output = body.stdout ? decodeBase64(body.stdout) : null;
-        this.error = body.stderr ? decodeBase64(body.error) : null;
+        this.error = body.stderr ? decodeBase64(body.stderr) : null;
         this.time = body.time;
         this.memory = body.memory;
+        this.status = body.status;
     }
 }
 
